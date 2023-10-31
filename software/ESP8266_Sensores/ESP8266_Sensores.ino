6f50aa74edb4522ac7652bab3c3c8000f6e6355c
MaximilianoGarcia716 <96884302+MaximilianoGarcia716@users.noreply.github.com>
2023-10-31 11:48:18 -0300

    Add files via upload

---
+++ software/ESP8266_Sensores/ESP8266_Sensores.ino
@@ -0,0 +1,31 @@
+#include <OneWire.h>                 //Se importan las librerías
+#include <DallasTemperature.h>
+#define Pin 2                        //Se declara el pin donde se conectará la DATA
+
+OneWire ourWire(Pin);                //Se establece el pin declarado como bus para la comunicación OneWire
+DallasTemperature sensors(&ourWire); //Se llama a la librería DallasTemperature
+
+//Esta funcion se ejecuta solo una vez
+//al iniciarse el Arduino. Aqui se colocan
+//las instrucciones que tienen que ver con la configuracion.
+void setup() {
+  Serial.begin(9600); //Iniciamos el puerto serie
+  //para comunicarse con la PC
+  //a una velocidad de 9600 bauds.
+  sensors.begin();                     //Se inician los sensores
+}
+
+void loop() {
+  int Valor_Humedad = analogRead(A0); //Lee el valor del conversor A/D del sensor de humedad
+  float Humedad=(645-Valor_Humedad)*100/(645-278); //Ajustamos la escala con 645=0% (Aire) y 278=100% (Agua) de humedad
+
+  sensors.requestTemperatures();       //Prepara el sensor de temperatura para la lectura
+  float Temperatura = sensors.getTempCByIndex(0); //Se obtiene el dato de temperatura
+  
+  Serial.print("Humedad: ");
+  Serial.println(Humedad); //Se lee e imprime el dato del sensor de humedad
+  Serial.print(Temperatura); //Se lee e imprime la temperatura en grados Centigrados
+  Serial.println(" Grados Centigrados");
+
+  delay(1000);                         //Se provoca una parada de 1 segundo antes de la próxima lectura
+}
